5bbac64ee9c6d0dd713f1c1433c99154eebd4d8a
bakmenson <bakmenson@gmail.com>
2020-02-18 22:16:01 +0300

    added script for cp dotfiles

---
+++ cp_dotfiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ~/
+
+cp ~/dotfiles/.zshrc ~/
+cp ~/dotfiles/compton.conf ~/.config
+cp ~/dotfiles/.ideavimrc ~/
+cp -r ~/dotfiles/i3 ~/.config
+cp -r ~/dotfiles/polybar ~/.config
+cp -r ~/dotfiles/mpv ~/.config
+cp -r ~/dotfiles/nvim ~/.config
+cp -r ~/dotfiles/rofi ~/.config
+cp -r ~/dotfiles/.gitconfig ~/
+cp -r ~/dotfiles/.gitignore_global ~/
+cp -r ~/dotfiles/.fonts ~/
+cp -r ~/dotfiles/xfce4/terminal ~/.config/xfce4
