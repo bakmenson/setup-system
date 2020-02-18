33ff9c3d13cf8479ceb2715023a5ed4f956015a2
bakmenson <bakmenson@gmail.com>
2020-02-18 18:50:13 +0300

    apt-get -> apt

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -8,4 +8,4 @@ curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt-get install yarn
+sudo apt install yarn
