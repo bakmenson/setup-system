4396f8dbd3a6681b8c9cb90163c50ac743c22690
bakmenson <bakmenson@gmail.com>
2020-02-19 08:43:39 +0300

    create symbol links instead of copy configs

---
+++ symlink_dotfiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ~/
+
+ln -s ~/dotfiles/.zshrc ~/
+ln -s ~/dotfiles/compton.conf ~/.config
+ln -s ~/dotfiles/.ideavimrc ~/
+ln -s ~/dotfiles/i3 ~/.config
+ln -s ~/dotfiles/polybar ~/.config
+ln -s ~/dotfiles/mpv ~/.config
+ln -s ~/dotfiles/nvim ~/.config
+ln -s ~/dotfiles/rofi ~/.config
+ln -s ~/dotfiles/.gitconfig ~/
+ln -s ~/dotfiles/.gitignore_global ~/
+ln -s ~/dotfiles/.fonts ~/
+ln -s ~/dotfiles/xfce4/terminal ~/.config/xfce4
