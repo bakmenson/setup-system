81bbd67bebcd8e6891b4acef66cde1006cc49fa9
bakmenson <bakmenson@gmail.com>
2020-04-11 12:02:14 +0300

    replace autologin in else if ubuntu

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+os_name="$(lsb_release -ds | cut -d' ' -f 1)"
+
 if [ ! -d ~/.config ]; then mkdir ~/.config; fi
 
 if [ -d ~/.config/polybar ]; then rm -rf ~/.config/polybar; fi
@@ -10,7 +12,6 @@ if [ -d ~/.config/vifm ]; then rm -rf ~/.config/vifm; fi
 if [ -d ~/.config/rofi ]; then rm -rf ~/.config/rofi; fi
 if [ -d ~/.config/xfce4/terminal ]; then rm -rf ~/.config/xfce4/terminal; fi
 if [ -d ~/.fonts ]; then rm -rf ~/.fonts; fi
-if [ -d /etc/sddm.conf.d ]; then sudo rm -rf /etc/sddm.conf.d; fi
 if [ -e ~/.config/compton.conf ]; then rm ~/.config/compton.conf; fi
 if [ -e ~/.zshrc ]; then rm ~/.zshrc; fi
 if [ -e ~/.ideavimrc ]; then rm ~/.ideavimrc; fi
@@ -26,23 +27,24 @@ ln -sf ~/dotfiles/vifm ~/.config
 ln -sf ~/dotfiles/rofi ~/.config
 ln -sf ~/dotfiles/xfce4 ~/.config/xfce4
 ln -sf ~/dotfiles/.fonts ~/
-
-sudo mkdir /etc/sddm.conf.d
-sudo ln -sf ~/dotfiles/autologin.conf /etc/sddm.conf.d
-
 ln -sf ~/dotfiles/compton.conf ~/.config
 ln -sf ~/dotfiles/.zshrc ~/
 ln -sf ~/dotfiles/.ideavimrc ~/
 ln -sf ~/dotfiles/.gitconfig ~/
 ln -sf ~/dotfiles/.gitignore_global ~/
 
-# ufetch
-os_name="$(lsb_release -ds | cut -d' ' -f 1)"
-
 if [[ $os_name == "Ubuntu" ]]; then
 	ln -sf ~/dotfiles/.ufetch-ubuntu ~/
 	mv ~/.ufetch-ubuntu ~/.ufetch
 else
+	# for arch lunux
+
+	# ufetch
 	ln -sf ~/dotfiles/.ufetch-arch ~/
 	mv ~/.ufetch-arch ~/.ufetch
+
+	# autologin sddm
+	if [ -d /etc/sddm.conf.d ]; then sudo rm -rf /etc/sddm.conf.d; fi
+	sudo mkdir /etc/sddm.conf.d
+	sudo ln -sf ~/dotfiles/autologin.conf /etc/sddm.conf.d
 fi
