5aa167b26e89b8e7cceba88650d5c49919ff6073
bakmenson <bakmenson@gmail.com>
2020-04-15 20:32:10 +0300

    changed install oh-my-zsh

--- setup_system.sh
+++ setup_system.sh
@@ -63,7 +63,7 @@ fi
 
 # oh-my-zsh
 if [ ! -d ~/.oh-my-zsh ]; then
-	sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 	git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
