e2fe406662881592cba3218624580564cca06ccd
bakmenson <bakmenson@gmail.com>
2020-11-04 22:24:19 +0300

    removed nvim

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -5,7 +5,6 @@ if [ ! -d ~/.config ]; then mkdir ~/.config; fi
 if [ -d ~/.config/polybar ]; then rm -rf ~/.config/polybar; fi
 if [ -d ~/.config/i3 ]; then rm -rf ~/.config/i3; fi
 if [ -d ~/.config/mpv ]; then rm -rf ~/.config/mpv; fi
-if [ -d ~/.config/nvim ]; then rm -rf ~/.config/nvim; fi
 if [ -d ~/.config/vifm ]; then rm -rf ~/.config/vifm; fi
 if [ -d ~/.config/rofi ]; then rm -rf ~/.config/rofi; fi
 if [ -d ~/.config/xfce4/terminal ]; then rm -rf ~/.config/xfce4/terminal; fi
@@ -27,7 +26,6 @@ mkdir ~/.config/nvim
 ln -sf ~/dotfiles/polybar ~/.config/polybar
 ln -sf ~/dotfiles/i3 ~/.config
 ln -sf ~/dotfiles/mpv ~/.config
-ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim
 ln -sf ~/dotfiles/vifm ~/.config
 ln -sf ~/dotfiles/rofi ~/.config
 ln -sf ~/dotfiles/qutebrowser ~/.config
