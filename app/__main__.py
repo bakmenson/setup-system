fa3a32c7f9a3fee63d60e7084211b251bd9b0ce3
bakmenson <bakmenson@gmail.com>
2021-05-25 17:00:11 +0300

    replace chdir into home

--- app/__main__.py
+++ app/__main__.py
@@ -9,8 +9,10 @@ print(repo_links)
 print(commands)
 print(dotfile_links)
 
-# chdir(str(Path.home()) + "/some")
-#
+# run(["sudo", "pacman", "-S", "--needed", "-", "<", PACKAGES])
+
+# chdir(str(Path.home()))
+
 # for data in repos_data:
 #     run(["git", "clone", f"https://github.com/{data.source}.git", data.dest])
 #     if data.action:
@@ -19,9 +21,9 @@ print(dotfile_links)
 #         # run(["sudo", "make", data.action])
 #         chdir(str(Path.home()) + "/some")
 
+# chdir(str(Path.home()))
+
 # for command in commands:
 #     run([*command.command])
-
-# run(["sudo", "pacman", "-S", "--needed", "-", "<", PACKAGES])
 
 # install_from_source(repos_data)
