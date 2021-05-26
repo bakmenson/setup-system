7158a7d75d3d333d6fb578f40f6194293e1331fa
bakmenson <bakmenson@gmail.com>
2021-05-26 15:58:16 +0300

    added exit() if command return code != 0

--- app/__main__.py
+++ app/__main__.py
@@ -3,7 +3,7 @@ from os import chdir, mkdir, makedirs
 from os.path import expanduser
 from pathlib import Path
 
-from app.data import additions, git_repos, dotfiles_paths, old_dotfiles_paths,\
+from app.data import additions, git_repos, dotfiles_paths, old_dotfiles_paths, \
     PACKAGES, packages
 
 # TODO: fix installing packages from a file as a list
@@ -16,28 +16,34 @@ run(["sudo", "pacman", "-S", "--needed", *packages])
 chdir(str(Path.home()))
 
 for git_repo in git_repos:
-    run(["git", "clone",
-         f"https://github.com/{git_repo.source}.git",
-         git_repo.dest])
+    command = run(["git", "clone",
+                   f"https://github.com/{git_repo.source}.git",
+                   git_repo.dest])
+    exit() if command.returncode != 0 else None
+
     if git_repo.action:
         chdir(git_repo.dest)
-        run(["sudo", "make", git_repo.action])
+        command = run(["sudo", "make", git_repo.action])
+        exit() if command.returncode != 0 else None
         chdir(str(Path.home()))
 
 for addition in additions:
     for i, elem in enumerate(addition.command):
         if elem.startswith("~"):
             addition.command[i] = expanduser(elem)
-    run([*addition.command])
+    command = run([*addition.command])
+    exit() if command.returncode != 0 else None
 
 for old_dotfile_path in old_dotfiles_paths:
     if Path(expanduser(old_dotfile_path)).exists():
         if Path(expanduser(old_dotfile_path)).is_dir():
-            run(["sudo", "rm", "-rf", expanduser(old_dotfile_path)])
+            command = run(["sudo", "rm", "-rf", expanduser(old_dotfile_path)])
         else:
-            run(["sudo", "rm", expanduser(old_dotfile_path)])
+            command = run(["sudo", "rm", expanduser(old_dotfile_path)])
+        exit() if command.returncode != 0 else None
 
 for dotfile_path in dotfiles_paths:
     if not Path(expanduser(dotfile_path.dest)).exists():
         makedirs(expanduser(dotfile_path.dest))
-    run(["ln", "-sf", dotfile_path.source, expanduser(dotfile_path.dest)])
+    command = run(["ln", "-sf", dotfile_path.source, expanduser(dotfile_path.dest)])
+    exit() if command.returncode != 0 else None
