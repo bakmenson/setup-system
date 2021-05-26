d99e8b2e03705cd8428f002ba54c8c31d29205a7
bakmenson <bakmenson@gmail.com>
2021-05-26 17:06:16 +0300

    removed link to dotfiles repo

--- app/__main__.py
+++ app/__main__.py
@@ -8,42 +8,44 @@ from app.data import additions, git_repos, dotfiles_paths, old_dotfiles_paths, \
 
 # TODO: fix installing packages from a file as a list
 # run(["sudo", "pacman", "-S", "--needed", "-", "<", PACKAGES])
-run(["sudo", "pacman", "-S", "--needed", *packages])
+# run(["sudo", "pacman", "-S", "--needed", *packages])
 # command = run(["sudo", "pacman", "-S", "--needed", *packages])
 # if command.returncode != 0:
 #     exit()
 
-chdir(str(Path.home()))
+print(packages)
 
-for git_repo in git_repos:
-    command = run(["git", "clone",
-                   f"https://github.com/{git_repo.source}.git",
-                   git_repo.dest])
-    exit() if command.returncode != 0 else None
+# chdir(str(Path.home()))
 
-    if git_repo.action:
-        chdir(git_repo.dest)
-        command = run(["sudo", "make", git_repo.action])
-        exit() if command.returncode != 0 else None
-        chdir(str(Path.home()))
+# for git_repo in git_repos:
+#     command = run(["git", "clone",
+#                    f"https://github.com/{git_repo.source}.git",
+#                    git_repo.dest])
+#     exit() if command.returncode != 0 else None
 
-for addition in additions:
-    for i, elem in enumerate(addition.command):
-        if elem.startswith("~"):
-            addition.command[i] = expanduser(elem)
-    command = run([*addition.command])
-    exit() if command.returncode != 0 else None
+    # if git_repo.action:
+    #     chdir(git_repo.dest)
+    #     command = run(["sudo", "make", git_repo.action])
+    #     exit() if command.returncode != 0 else None
+    #     chdir(str(Path.home()))
 
-for old_dotfile_path in old_dotfiles_paths:
-    if Path(expanduser(old_dotfile_path)).exists():
-        if Path(expanduser(old_dotfile_path)).is_dir():
-            command = run(["sudo", "rm", "-rf", expanduser(old_dotfile_path)])
-        else:
-            command = run(["sudo", "rm", expanduser(old_dotfile_path)])
-        exit() if command.returncode != 0 else None
+# for addition in additions:
+#     for i, elem in enumerate(addition.command):
+#         if elem.startswith("~"):
+#             addition.command[i] = expanduser(elem)
+#     command = run([*addition.command])
+#     exit() if command.returncode != 0 else None
 
-for dotfile_path in dotfiles_paths:
-    if not Path(expanduser(dotfile_path.dest)).exists():
-        makedirs(expanduser(dotfile_path.dest))
-    command = run(["ln", "-sf", dotfile_path.source, expanduser(dotfile_path.dest)])
-    exit() if command.returncode != 0 else None
+# for old_dotfile_path in old_dotfiles_paths:
+#     if Path(expanduser(old_dotfile_path)).exists():
+#         if Path(expanduser(old_dotfile_path)).is_dir():
+#             command = run(["sudo", "rm", "-rf", expanduser(old_dotfile_path)])
+#         else:
+#             command = run(["sudo", "rm", expanduser(old_dotfile_path)])
+#         exit() if command.returncode != 0 else None
+
+# for dotfile_path in dotfiles_paths:
+#     if not Path(expanduser(dotfile_path.dest)).exists():
+#         makedirs(expanduser(dotfile_path.dest))
+#     command = run(["ln", "-sf", dotfile_path.source, expanduser(dotfile_path.dest)])
+#     exit() if command.returncode != 0 else None
