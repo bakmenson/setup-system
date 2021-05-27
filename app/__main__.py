a561dbe9c41dbfaa193ed970bc2468df1d2ef7cf
bakmenson <bakmenson@gmail.com>
2021-05-27 15:44:55 +0300

    added check if run return non-zero value

--- app/__main__.py
+++ app/__main__.py
@@ -6,25 +6,19 @@ from pathlib import Path
 from app.data import additions, git_repos, dotfiles_paths, old_dotfiles_paths, \
     PACKAGES, packages
 
-run(["sudo pacman -S --needed - < " + PACKAGES], shell=True, check=True, text=True)
-run(["sudo", "pacman", "-S", "--needed", "-", "<", PACKAGES])
-run(["sudo", "pacman", "-S", "--needed", *packages])
-command = run(["sudo", "pacman", "-S", "--needed", *packages])
-if command.returncode != 0:
-    exit()
+run(["sudo pacman -S --needed - < " + PACKAGES],
+    shell=True, check=True, text=True)
 
 chdir(str(Path.home()))
 
 for git_repo in git_repos:
-    command = run(["git", "clone",
-                   f"https://github.com/{git_repo.source}.git",
-                   git_repo.dest])
-    exit() if command.returncode != 0 else None
+    run(["git", "clone",
+            f"https://github.com/{git_repo.source}.git",
+            git_repo.dest], check=True, text=True)
 
     if git_repo.action:
         chdir(git_repo.dest)
-        command = run(["sudo", "make", git_repo.action])
-        exit() if command.returncode != 0 else None
+        run(["sudo", "make", git_repo.action], check=True, text=True)
         chdir(str(Path.home()))
 
 for addition in additions:
@@ -36,21 +30,21 @@ for addition in additions:
         if elem in (" - ", " < ", " > ", " | "):
             is_spec_symbol = True
     if is_spec_symbol:
-        command = run([" ".join(addition.command)], shell=True, check=True, text=True)
+        run([" ".join(addition.command)], shell=True, check=True, text=True)
     else:
-        command = run([*addition.command])
-        exit() if command.returncode != 0 else None
+        run([*addition.command], check=True, text=True)
 
 for old_dotfile_path in old_dotfiles_paths:
     if Path(expanduser(old_dotfile_path)).exists():
         if Path(expanduser(old_dotfile_path)).is_dir():
-            command = run(["sudo", "rm", "-rf", expanduser(old_dotfile_path)])
+            run(["sudo", "rm", "-rf", expanduser(old_dotfile_path)],
+                check=True, text=True)
         else:
-            command = run(["sudo", "rm", expanduser(old_dotfile_path)])
-        exit() if command.returncode != 0 else None
+            run(["sudo", "rm", expanduser(old_dotfile_path)],
+                check=True, text=True)
 
 for dotfile_path in dotfiles_paths:
     if not Path(expanduser(dotfile_path.dest)).exists():
         makedirs(expanduser(dotfile_path.dest))
-    command = run(["ln", "-sf", dotfile_path.source, expanduser(dotfile_path.dest)])
-    exit() if command.returncode != 0 else None
+    run(["ln", "-sf", dotfile_path.source, expanduser(dotfile_path.dest)],
+        check=True, text=True)
