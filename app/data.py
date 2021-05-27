9144f6f552cf31b257adda6552b6f401eb0ff40f
bakmenson <bakmenson@gmail.com>
2021-05-27 15:46:45 +0300

    removed variable packages

--- app/data.py
+++ app/data.py
@@ -25,6 +25,3 @@ dotfiles_paths: list[DotfileLinks] = list(
 old_dotfiles_paths: list = list(
     map(lambda o: o.strip(), read_data(OLD_DOTFILES))
 )
-packages: list = list(
-    map(lambda p: p.strip(), read_data(PACKAGES))
-)
