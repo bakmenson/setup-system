0d1513e9194cccabcbdbee54ef8dce28708bec97
bakmenson <bakmenson@gmail.com>
2022-01-13 22:10:35 +0300

    changed preparing commands

--- setup_system/__main__.py
+++ setup_system/__main__.py
@@ -23,32 +23,54 @@ needed_packages: list[NeededPackage] = ps.get_needed_packages(
 needed_packages.sort(key=lambda package: package.priority)
 
 commands: list[str] = list()
-args: list[str] = []
+split_commands: list[list[str]] = []
 
 for command, mode, path, _ in needed_packages:
     for item in ps.form_install_command(command, read(path), mode):
-        args = split(item)
-        if "cd" == args[0]:
-            chdir(ps.expand_path(args[1]))
-            args = []
-        elif "mkdir" == args[0]:
-            mkdir(ps.expand_path(args[1]))
-            args = []
-        elif "git" == args[0]:
-            args = ps.form_git_command(args)
-            if Path(args[-1]).exists():
-                rmtree(args[-1])
-        if args:
+        split_commands.append(split(item))
+
+for split_command in split_commands:
+    for item in split_command:
+        if item.startswith("~"):
+            new_item = Path.expanduser(PosixPath(item))
+            item_idx = split_command.index(item)
+            split_command.insert(item_idx, str(new_item))
+            split_command.remove(item)
+
+    if "cd" == split_command[0]:
+        if Path(split_command[1]).exists():
+            chdir(split_command[1])
+        split_command = None
+    elif "mkdir" == split_command[0]:
+        if not Path(split_command[1]).exists():
+            mkdir(split_command[1])
+        split_commands.remove(split_command)
+        split_command = None
+    elif "rm" == split_command[0]:
+        path_to_rm: Path = Path.home().joinpath(Path(split_command[-1]))
+        if path_to_rm.exists():
+            if path_to_rm.is_dir():
+                rmtree(path_to_rm)
+            else:
+                Path.unlink(path_to_rm)
+        split_command = None
+    elif "git" == split_command[0] and len(split_command) == 3:
+        clone_dest = Path.home().joinpath(Path(split_command[2]).stem)
+        if clone_dest.exists():
+            rmtree(clone_dest)
+        split_command.append(str(clone_dest))
+
+    if split_command is not None:
+        try:
+            run(split_command, check=True, stderr=PIPE)
+        except CalledProcessError as e:
             try:
-                run(args, check=True, stderr=PIPE)
+                run(" ".join(split_command), shell=True, check=True, stderr=PIPE, encoding="utf-8")
             except CalledProcessError as e:
-                try:
-                    run(" ".join(args), shell=True, check=True, stderr=PIPE)
-                except CalledProcessError as e:
-                    if not e.stderr:
-                        exit(1)
-                    print(f"\n{'=' * 50}\n{e.stderr}")
-                    exit(e.args[0])
-            except OSError as e:
-                print(e)
-                exit(1)
+                if not e.stderr:
+                    exit(1)
+                print(f"\n{'=' * 50}\n{e.stderr}")
+                exit(e.args[0])
+        except OSError as e:
+            print(e)
+            exit(1)
