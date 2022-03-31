2fcb993862b105bb9e9666b270f8525f7e6a2dec
bakmenson <bakmenson@gmail.com>
2022-03-31 21:49:06 +0300

    use zsh instead of bash

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/zsh
 
 emacs --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "setup-system/README.org")'
-bash setup-system/setup-system.sh
+zsh setup-system/setup-system.sh
 rm setup-system/setup-system.sh
 mv ~/setup-system ~/dev/setup-system
