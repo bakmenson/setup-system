f5cdb48e86eec9e5dab6c7f0953644063398e3bb
bakmenson <bakmenson@gmail.com>
2022-04-26 22:36:28 +0300

    added remove update.sh and pre-install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 emacs --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "setup-system/README.org")'
 zsh setup-system/setup-system.sh
+rm update.sh
+rm pre-install.sh
 rm setup-system/setup-system.sh
 mv ~/setup-system ~/dev/setup-system
 echo "\n\nDone"
