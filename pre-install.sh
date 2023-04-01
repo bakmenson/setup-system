352a1f4eb009cd0486d94ab9e0477d37be8242cf
bakmenson <bakmenson@gmail.com>
2023-04-01 17:45:39 +0300

    remove fish

--- pre-install.sh
+++ pre-install.sh
@@ -2,12 +2,10 @@
 
 sudo pacman -Syu
 
-sudo pacman -S emacs fish
+sudo pacman -S emacs
 
 mkdir -p ~/dev
 
 emacs --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "setup-system/README.org")'
-
-chsh -s /bin/fish
 
 reboot
