cf0c9e9b7cdf8e3365b59f6300c93be4e8396343
bakmenson <bakmenson@gmail.com>
2023-04-02 16:24:21 +0300

    fix fish and automount hdd

--- pre-install.sh
+++ pre-install.sh
@@ -2,10 +2,12 @@
 
 sudo pacman -Syu
 
-sudo pacman -S emacs
+sudo pacman -S --needed emacs fish
 
 mkdir -p ~/dev
 
 emacs --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "setup-system/README.org")'
+
+chsh -s /bin/fish
 
 reboot
